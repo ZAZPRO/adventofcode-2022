9467fcb5f720b7e4eabb1070d1daa1909a1a101b
Daniil Mira <contact@daniilmira.com>
2022-12-02 11:04:46 +0100

    Added: Solved Day 2!

---
+++ 02/rust/src/bin/part_one.rs
@@ -0,0 +1,113 @@
+use std::error::Error;
+
+// Enum that describes match result.
+enum Outcome {
+    Win,
+    Draw,
+    Lose,
+}
+
+// Trait for outcome to calculate result score.
+impl Outcome {
+    pub fn get_score(&self) -> u32 {
+        match &self {
+            Outcome::Win => 6,
+            Outcome::Draw => 3,
+            Outcome::Lose => 0,
+        }
+    }
+}
+
+// Possible Items that players can throw.
+enum Item {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+// Value of each Item.
+impl Item {
+    pub fn get_score(&self) -> u32 {
+        match &self {
+            Item::Rock => 1,
+            Item::Paper => 2,
+            Item::Scissors => 3,
+        }
+    }
+}
+
+// Struct that holds each game Round.
+struct Round {
+    opponent: Item,
+    player: Item,
+}
+
+impl Round {
+    // Common Rust way to create structs.
+    pub fn new(opponent: Item, player: Item) -> Self {
+        Self { opponent, player }
+    }
+
+    // Calculate outcome of the Round.
+    pub fn get_outcome(&self) -> Outcome {
+        match (&self.player, &self.opponent) {
+            (Item::Rock, Item::Scissors) => Outcome::Win,
+            (Item::Rock, Item::Paper) => Outcome::Lose,
+            (Item::Paper, Item::Rock) => Outcome::Win,
+            (Item::Paper, Item::Scissors) => Outcome::Lose,
+            (Item::Scissors, Item::Paper) => Outcome::Win,
+            (Item::Scissors, Item::Rock) => Outcome::Lose,
+            _ => Outcome::Draw,
+        }
+    }
+
+    // Calculate score of the round.
+    pub fn calc_score(&self) -> u32 {
+        &self.player.get_score() + &self.get_outcome().get_score()
+    }
+}
+
+// Program entry point.
+fn main() -> Result<(), Box<dyn Error>> {
+    // List of game rounds.
+    let mut rounds: Vec<Round> = Vec::new();
+
+    // Read input file into String.
+    let file = std::fs::read_to_string("../input.txt")?;
+    // For each line in the file.
+    for line in file.lines() {
+        // Split a line in two values.
+        let mut iter = line.split_whitespace();
+
+        // First value is what current opponent is throwing.
+        let current_opponent = match iter.next() {
+            Some(v) => match v {
+                "A" => Item::Rock,
+                "B" => Item::Paper,
+                "C" => Item::Scissors,
+                _ => todo!(),
+            },
+            None => todo!(),
+        };
+
+        // Second value is what current player is throwing.
+        let current_player = match iter.next() {
+            Some(v) => match v {
+                "X" => Item::Rock,
+                "Y" => Item::Paper,
+                "Z" => Item::Scissors,
+                _ => todo!(),
+            },
+            None => todo!(),
+        };
+
+        // Create a new Round struct with opponent and player values.
+        rounds.push(Round::new(current_opponent, current_player));
+    }
+
+    // Calculate a final sum by calling a score calculation method of Round struct and summing those up.
+    let final_sum: u32 = rounds.into_iter().map(|r| r.calc_score()).sum();
+    println!("Final Score: {final_sum}");
+
+    Ok(())
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Advent of Code 2022
+# 🎄 Advent of Code 2022
 
 Solved, but unoptimized solutions. First year of trying to do those challanges!
 
@@ -9,3 +9,7 @@ In the ```rust``` directory run:
 cargo run --bin part_one
 cargo run --bin part_two
 ```
+
+# Solutions
+- Day 01: Calorie Counting [[One](https://github.com/ZAZPRO/adventofcode-2022/blob/main/01/rust/src/bin/part_one.rs), [Two](https://github.com/ZAZPRO/adventofcode-2022/blob/main/01/rust/src/bin/part_two.rs)]
+- Day 02: Rock Paper Scissors [[One](https://github.com/ZAZPRO/adventofcode-2022/blob/main/02/rust/src/bin/part_one.rs), [Two](https://github.com/ZAZPRO/adventofcode-2022/blob/main/02/rust/src/bin/part_two.rs)]
